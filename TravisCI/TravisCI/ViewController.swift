908da4360cebcf51e52f61f5256e34fbc7466f8b
Ben Ormos <bendesign555@gmail.com>
2020-01-16 06:57:18 +0000

    Created a new scheme for the CI server

---
+++ TravisCI/TravisCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravisCI
+//
+//  Created by Ben Ormos on 16/01/2020.
+//  Copyright © 2020 Ben Ormos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
